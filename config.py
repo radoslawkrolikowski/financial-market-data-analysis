6f796474d0c2dbd3d361bbafc44c01cf27b0736d
root <root@localhost.localdomain>
2019-12-04 15:57:02 +0100

    Add configuration file

---
+++ config.py
@@ -0,0 +1,17 @@
+"""Configuration file.
+
+"""
+import pytz
+
+tokens = {'av_token': '6CK0W5ZFDAUWX0DQ',
+          'iex_token': 'pk_e01ee438a3b545818531ea9d669ddee0'}
+
+
+time_zone = {'EST': pytz.timezone('US/Eastern'),
+    'UTC': pytz.timezone('UTC'),
+    'iex_stock': pytz.timezone('US/Eastern'),
+    'iex_forex': pytz.timezone('UTC')}
+
+topic = ['main', 'economic', 'news']
+
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0'
