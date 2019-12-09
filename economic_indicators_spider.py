00f584045cdbb48c22007e66bd89ccbb5b6a1163
root <root@localhost.localdomain>
2019-12-09 19:15:30 +0100

    Implement crawler script that utilizes 'billiard' to run the spider multiple times

--- economic_indicators_spider.py
+++ economic_indicators_spider.py
@@ -1,23 +1,65 @@
 # -*- coding: utf-8 -*-
+from billiard import Process
 from scrapy import Spider
-from scrapy.crawler import CrawlerProcess
+from scrapy import signals as scrapy_signals
+from scrapy.crawler import Crawler
+from twisted.internet import reactor
 from config import user_agent, time_zone
 from datetime import datetime
+from collections import defaultdict
 import logging
+import re
+import pickle
+
+
+class IndicatorCollectorPipeline:
+    def __init__(self, producer, topic):
+        self.producer = producer
+        self.topic = topic
+        self.items_dict = defaultdict()
+
+        try:
+            with open(r"items.pickle", "rb") as output_file:
+                self.prev_items = pickle.load(output_file)
+        except (OSError, IOError):
+            with open(r"items.pickle", "wb") as output_file:
+                pickle.dump({}, output_file)
+
+            self.prev_items = defaultdict()
+
+    def process_item(self, item, spider):
+        self.item = item
+
+        self.items_dict.setdefault((item['Schedule_datetime'], item['Event']), item)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(producer=crawler.spider.producer,
+            topic=crawler.spider.topic)
+
+    def close_spider(self, spider):
+        new_items = [self.items_dict[k] for k in set(self.items_dict) - set(self.prev_items)]
+
+        if new_items:
+            # Send economic data through kafka producer
+            self.producer.send(topic=self.topic, value=new_items)
+
+        with open(r"items.pickle", "wb") as output_file:
+            pickle.dump(self.items_dict, output_file)
 
 
 class EconomicIndicatorsSpiderSpider(Spider):
 
     name = 'economic_indicators_spider'
     allowed_domains = ['www.investing.com']
-    start_urls = ['http://www.investing.com/economic-calendar/']
+    start_urls = ['https://www.investing.com/economic-calendar/']
     custom_settings = {
         'ITEM_PIPELINES': {
             'economic_indicators_spider.IndicatorCollectorPipeline': 100
         }
     }
 
-    def __init__(self, countries, importance,  event_list, current_dt):
+    def __init__(self, countries, importance,  event_list, current_dt, producer, topic):
 
         super(EconomicIndicatorsSpiderSpider, self).__init__()
 
@@ -25,6 +67,8 @@ class EconomicIndicatorsSpiderSpider(Spider):
         self.importance = ['bull' + x for x in importance]
         self.event_list = event_list
         self.current_dt = current_dt
+        self.producer = producer
+        self.topic = topic
 
     def parse(self, response):
 
@@ -37,9 +81,11 @@ class EconomicIndicatorsSpiderSpider(Spider):
             event_datetime = datetime.strptime(datetime_str, "%Y/%m/%d %H:%M:%S")
             event_datetime = event_datetime.replace(tzinfo=time_zone['EST'])
 
+            current_dt_str = datetime.strftime(self.current_dt, "%Y/%m/%d %H:%M:%S")
+
             # Return only events that passed
-            # if not self.current_dt >= event_datetime:
-            #     continue
+            if not self.current_dt >= event_datetime:
+                continue
 
             country = event.xpath(".//td/span/@title").extract_first()
 
@@ -55,11 +101,13 @@ class EconomicIndicatorsSpiderSpider(Spider):
 
             event_name = event.xpath(".//td[@class='left event']/a/text()").extract_first()
             event_name = event_name.strip(' \r\n\t ')
+            event_name_regex = re.findall(r"(.*?)(?=.\([a-zA-Z]{3}\))", event_name)
 
-            if event_name not in self.event_list:
-                continue
+            if event_name_regex:
+                event_name = event_name_regex[0].strip()
 
-            print('EVENT NAME: ', event_name)
+            # if event_name not in self.event_list:
+            #     continue
 
             actual = event.xpath(".//td[contains(@id, 'eventActual')]/text()").extract_first().strip('%M BK')
 
@@ -75,7 +123,8 @@ class EconomicIndicatorsSpiderSpider(Spider):
             if forecast != '\xa0':
                 forecast_actual_diff = str(float(forecast) - float(actual))
 
-            yield {'Datetime': datetime_str,
+            yield {'Scrap_datetime': current_dt_str,
+                'Schedule_datetime': datetime_str,
                 'Event': event_name,
                 'Importance': importance_label[-1],
                 'Actual': actual,
@@ -85,33 +134,39 @@ class EconomicIndicatorsSpiderSpider(Spider):
                 'Forc_actual_diff': forecast_actual_diff if forecast != '\xa0' else None}
 
 
-indicators = []
 
+class CrawlerScript(Process):
+    def __init__(self, countries, importance, event_list, current_dt, producer, topic):
 
-class IndicatorCollectorPipeline:
-    def __init__(self):
-        self.ids_seen = set()
+        Process.__init__(self)
 
-    def process_item(self, item, spider):
-        indicators.append(item)
+        self.countries = countries
+        self.importance = importance
+        self.event_list = event_list
+        self.current_dt = current_dt
+        self.producer = producer
+        self.topic = topic
 
-
-class ItemsCrawler:
-    def __init__(self):
-
-        self.crawler = CrawlerProcess({
+        self.crawler = Crawler(
+          EconomicIndicatorsSpiderSpider,
+          settings={
             'USER_AGENT': user_agent,
-            'LOG_LEVEL': 'INFO'
-        })
+          }
+        )
 
-    def get_indicators(self, countries, importance, event_list, current_dt, start_crawler=False):
+        self.crawler.signals.connect(reactor.stop, signal=scrapy_signals.spider_closed)
 
-        self.crawler.crawl(EconomicIndicatorsSpiderSpider,
-              countries=countries, importance=importance, event_list=event_list, current_dt=current_dt)
+    def run(self):
+        self.crawler.crawl(self.countries, self.importance, self.event_list, self.current_dt,
+            self.producer, self.topic)
+        reactor.run()
 
-        # CrawlerProcess can be started only once
-        if start_crawler:
-            self.crawler.start()
 
-        return indicators
+def run_indicator_spider(countries, importance, event_list, current_dt, producer, topic):
+
+    crawler = CrawlerScript(countries, importance, event_list, current_dt, producer, topic)
+
+    # the script will block here until the crawling is finished
+    crawler.start()
+    crawler.join()
 
