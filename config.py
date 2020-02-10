61401f8e681a2bb0505196b761a886ac2ce9216c
root <root@localhost.localdomain>
2020-02-10 14:14:32 +0100

    Add economic indicator spider empty message template. Add mysql login data

--- config.py
+++ config.py
@@ -4,17 +4,30 @@
 import pytz
 
 tokens = {'av_token': '6CK0W5ZFDAUWX0DQ',
-          'iex_token': 'pk_e01ee438a3b545818531ea9d669ddee0'}
-
+    'iex_token': 'pk_e01ee438a3b545818531ea9d669ddee0'}
 
 time_zone = {'EST': pytz.timezone('US/Eastern'),
     'UTC': pytz.timezone('UTC'),
     'iex_stock': pytz.timezone('US/Eastern'),
     'iex_forex': pytz.timezone('UTC')}
 
-kafka_config = {'servers':['localhost:9092'], 'topics': ['main', 'economic', 'news']}
+kafka_config = {'servers':['localhost:9092'], 'topics': ['vix', 'volume', 'cot', 'ind', 'deep']}
 
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0'
 
-event_list = ['Crude Oil Inventories', 'ISM Non-Manufacturing PMI', 'ISM Non-Manufacturing Employment', 'Federal Budget Balance',
-              'Services PMI', 'ADP Nonfarm Employment Change', 'Core CPI', 'CPI', 'Fed Interest Rate Decision']
+mysql_user = 'admin'
+
+mysql_password = 'admin'
+
+event_list = ['Crude Oil Inventories', 'ISM Non-Manufacturing PMI', 'ISM Non-Manufacturing Employment',
+    'Services PMI', 'ADP Nonfarm Employment Change', 'Core CPI', 'Fed Interest Rate Decision', 'Building Permits',
+    'Core Retail Sales', 'Retail Sales', 'JOLTs Job Openings', 'Nonfarm Payrolls', 'Unemployment Rate']
+
+temp_event_list = [event_name.replace(" ", "_") for event_name in event_list]
+event_values = ["Actual", "Prev_actual_diff", "Forc_actual_diff"]
+empty_ind_dict = {"Timestamp": None}
+
+for event in temp_event_list:
+    empty_ind_dict.setdefault(event, {})
+    for value in event_values:
+        empty_ind_dict[event].setdefault(value, None)
